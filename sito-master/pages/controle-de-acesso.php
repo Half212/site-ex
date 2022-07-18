493879bd9b2f56dbde48306a2fba5631a7dfa2d9
Half212 <53454449+Half212@users.noreply.github.com>
2022-07-18 16:29:59 -0300

    Initial commit

---
+++ sito-master/pages/controle-de-acesso.php
@@ -0,0 +1,109 @@
+<!-- End Header Area -->
+
+<!-- Start Page Title Area -->
+
+<section class="page-title-area bg-cover1">
+
+    <div class="page-title-wrapper section-spacing">
+
+        <div class="page-title text-center white-text">
+
+            <h1 class="heading-1 wow fadeInUp" data-wow-durantio="1s">Controle de acesso</h1>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- End Page Title Area -->
+
+<!-- Start Topics  Area -->
+
+<div class="topics-area bg-color1">
+
+    <div class="topics-wrapper section-spacing pb-0">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-6 col-lg-4">
+
+                <article class="topics-service bg-color3 wow fadeInUp" data-wow-durantio="3s">
+
+                    <header class="topics-title">
+
+                        <h4 class="heading-4">Elaboração de projetos</h4>
+
+                    </header>
+
+                    <ul class="topics-list list-inline">
+
+                        <li><a href="#"></a></li>
+
+                        <li><a href="#"></a></li>
+
+                        <li><a href="#"></a></li>
+
+                    </ul>
+
+                </article>
+
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+
+                <article class="topics-service bg-color3 wow fadeInUp" data-wow-durantio="3s">
+
+                    <header class="topics-title">
+
+                        <h4 class="heading-4">Intalação de rede</h4>
+
+                    </header>
+
+                    <ul class="topics-list list-inline">
+
+                        <li><a href="#"></a></li>
+
+                        <li><a href="#"></a></li>
+
+                        <li><a href="#"></a></li>
+
+                    </ul>
+
+                </article>
+
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+
+                <article class="topics-service bg-color3 wow fadeInUp" data-wow-durantio="3s">
+
+                    <header class="topics-title">
+
+                        <h4 class="heading-4">Manutenção de rede</h4>
+
+                    </header>
+
+                    <ul class="topics-list list-inline">
+
+                        <li><a href="#"></a></li>
+
+                        <li><a href="#"></a></li>
+
+                        <li><a href="#"></a></li>
+
+                    </ul>
+
+                </article>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    </div>
+
+</div>
